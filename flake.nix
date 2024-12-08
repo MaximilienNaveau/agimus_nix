ee0edc5e4d588fe69361ca548c9e64965dc36110
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-12-08 16:16:40 +0100

    geptto-viewer crash

--- flake.nix
+++ flake.nix
@@ -3,19 +3,34 @@
     # from https://github.com/lopsided98/nix-ros-overlay
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
     nixpkgs.follows = "nix-ros-overlay/nixpkgs";  # IMPORTANT!!!
-    gepettopkgs.url = "github:Gepetto/nixpkgs/hpp";
+    original-nixpkgs.url = "github:NixOS/nixpkgs";
+    
   };
-  outputs = { self, nix-ros-overlay, nixpkgs, gepettopkgs }:
+  outputs = { self, nix-ros-overlay, nixpkgs, original-nixpkgs }:
     nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
       let
+        opkgs = import original-nixpkgs {
+          inherit system;
+        };
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ nix-ros-overlay.overlays.default ];
         };
-        geppkgs = import gepettopkgs {
-          inherit system;
-        };
       in {
+
+        # Local packages:
+        
+        # (pkgs.callPackage ./pkgs/gepetto-viewer/package.nix {})
+        # (pkgs.callPackage ./pkgs/gepetto-viewer-corba/package.nix {})
+        # (pkgs.callPackage ./pkgs/hpp-constraints/package.nix {})
+        # (pkgs.callPackage ./pkgs/hpp-core/package.nix {})
+        # (pkgs.callPackage ./pkgs/hpp-manipulation/package.nix {})
+        # (pkgs.callPackage ./pkgs/hpp-manipulation-urdf/package.nix {})
+        # (pkgs.callPackage ./pkgs/hpp-pinocchio/package.nix {})
+        # (pkgs.callPackage ./pkgs/hpp-statistics/package.nix {})
+        # (pkgs.callPackage ./pkgs/hpp-template-corba/package.nix {})
+        # (pkgs.callPackage ./pkgs/colmpc/package.nix {})
+
         devShells.default = pkgs.mkShell {
           name = "AGIMUS humble";
           # buildInputs = [
@@ -25,33 +40,13 @@
           packages = [
             # Basic build system for ROS
             pkgs.colcon
-            
-            # Local packages:
-            (pkgs.callPackage ./pkgs/qgv/package.nix {})
-            # (pkgs.callPackage ./pkgs/gepetto-viewer/package.nix { osgqt = pkgs.osgqt; })
-            # (pkgs.callPackage ./pkgs/gepetto-viewer/package.nix {})
-            # (pkgs.callPackage ./pkgs/gepetto-viewer-corba/package.nix {})
-            # (pkgs.callPackage ./pkgs/hpp-constraints/package.nix {})
-            # (pkgs.callPackage ./pkgs/hpp-core/package.nix {})
-            # (pkgs.callPackage ./pkgs/hpp-manipulation/package.nix {})
-            # (pkgs.callPackage ./pkgs/hpp-manipulation-urdf/package.nix {})
-            # (pkgs.callPackage ./pkgs/hpp-pinocchio/package.nix {})
-            # (pkgs.callPackage ./pkgs/hpp-statistics/package.nix {})
-            # (pkgs.callPackage ./pkgs/hpp-template-corba/package.nix {})
-            # (pkgs.callPackage ./pkgs/colmpc/package.nix {})
 
-
-            # # Python packages:
-            # (pkgs.python3.withPackages(p: [
-            #   # Development tools
-            #   p.ipython
-            #   # Gepetto/Inria packages.
-            #   p.crocoddyl
-            #   # p.mim_solvers
-            #   p.proxsuite
-            # ]))
-            
-            # # Gepetto packages from fork
+            # Gepetto packages from source
+            (pkgs.callPackage ./pkgs/gepetto-viewer/package.nix {
+              python3Packages=opkgs.python3Packages;
+              qgv=(pkgs.callPackage ./pkgs/qgv/package.nix {});
+              osgqt=opkgs.osgqt;
+            })
             # (geppkgs.python3.withPackages(p: [
             #   p.hpp-tutorial
             #   p.hpp-corbaserver
@@ -71,6 +66,16 @@
             # geppkgs.hpp-template-corba
             # # geppkgs.hpp-tools
             # geppkgs.hpp-util
+
+            # # Python packages:
+            # (pkgs.python3.withPackages(p: [
+            #   # Development tools
+            #   p.ipython
+            #   # Gepetto/Inria packages.
+            #   p.crocoddyl
+            #   # p.mim_solvers
+            #   p.proxsuite
+            # ]))
 
             # ROS packages
             (with pkgs.rosPackages.humble; buildEnv {
